54a0b629d23190fff49bf2c7469938618da36430
Aleksandr <99916190+AleksandrFisun@users.noreply.github.com>
2022-10-28 00:54:48 +0300

    1

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -4,8 +4,8 @@ const Loader = () => {
   return (
     <ColorRing
       visible={true}
-      height="80"
-      width="80"
+      height="40"
+      width="40"
       ariaLabel="blocks-loading"
       wrapperStyle={{}}
       wrapperClass="blocks-wrapper"
